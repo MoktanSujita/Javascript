fcc590a4eeac3ef8d252ed34beb1d824b83d71a2
Moktan Sujita <sujitamoktan77@gmail.com>
2024-09-11 22:55:54 +0545

    first commit

---
+++ javascript/first.js
@@ -0,0 +1,36 @@
+/*console.log("Hello World");
+const name="Sujita";
+const id= 4;
+const a=true;
+const date= new Date();//name of the object starts with uppercaseletter
+console.log(date,date.getDate);//get date ctrl+space 
+typeof(NaN);
+//** is power
+//Ways of defining variable
+const a=1;//block scope, value cannot be rewritten 
+let b=1;//not a block scope,cannot be rewritten
+var c=1;//global scope
+*/
+// if(true)
+// {
+//     var z=5;
+// }
+// console.log(z);
+
+/*let x=6;
+console.log(x);
+let x=8;
+console.log(x);
+const b=6;
+b++;
+console.log(b);
+H1 innerHTML="HE"*/
+//const h1=document.getElementsByTagName("h1");
+/*const h1=document.getElementById("Moktan");
+h1.innerHTML="llllll";
+
+console.log(h1);*/
+//web ma date current
+const date = new Date();
+console.log(date, date.getDate);
+document.getElementById
