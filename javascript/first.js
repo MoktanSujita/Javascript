51f37d795d5271d4ee2325b49edbf8ff7a4f2e49
Sujita Moktan <160257525+MoktanSujita@users.noreply.github.com>
2024-09-15 23:32:48 +0545

    first.js

--- javascript/first.js
+++ javascript/first.js
@@ -1,4 +1,4 @@
-/*console.log("Hello World");
+console.log("Hello World");
 const name="Sujita";
 const id= 4;
 const a=true;
@@ -10,14 +10,14 @@ typeof(NaN);
 const a=1;//block scope, value cannot be rewritten 
 let b=1;//not a block scope,cannot be rewritten
 var c=1;//global scope
-*/
-// if(true)
-// {
-//     var z=5;
-// }
-// console.log(z);
 
-/*let x=6;
+ if(true)
+ {
+     var z=5;
+ }
+ console.log(z);
+
+let x=6;
 console.log(x);
 let x=8;
 console.log(x);
@@ -25,12 +25,12 @@ const b=6;
 b++;
 console.log(b);
 H1 innerHTML="HE"*/
-//const h1=document.getElementsByTagName("h1");
+const h1=document.getElementsByTagName("h1");
 /*const h1=document.getElementById("Moktan");
 h1.innerHTML="llllll";
 
 console.log(h1);*/
-//web ma date current
+//Show current date current
 const date = new Date();
 console.log(date, date.getDate);
 document.getElementById
